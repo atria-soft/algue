c5b5d76eaeef1902d4843985fb0315b7dac1fc18
Edouard DUPIN <yui.heero@gmail.com>
2016-10-23 23:20:07 +0200

    [DOC] correct the doxygen display

--- search/all_1.js
+++ search/all_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['b',['b',['http://atria-soft.github.io/etk/classetk_1_1_color.html#a723f8b355c03aa7c1cefa4cdfe34e68d',1,'etk::Color']]],
+  ['b',['b',['http://atria-soft.github.io/etk/classetk_1_1_color.html#ac6be77ac33983d040a2ede58bc631d94',1,'etk::Color']]],
   ['base64_2ehpp',['base64.hpp',['../base64_8hpp.html',1,'']]],
   ['basenoise',['BaseNoise',['http://atria-soft.github.io/etk/classetk_1_1_base_noise.html',1,'etk']]],
   ['basenoise',['BaseNoise',['http://atria-soft.github.io/etk/classetk_1_1_base_noise.html#ad5cbfcc2d967af185c264744de04cf15',1,'etk::BaseNoise']]],
--- search/files_2.js
+++ search/files_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['color_2ehpp',['Color.hpp',['http://atria-soft.github.io/etk/_color_8hpp.html',1,'']]]
+  ['color_2ehpp',['Color.hpp',['http://atria-soft.github.io/etk/__color__8hpp.html',1,'']]]
 ];
